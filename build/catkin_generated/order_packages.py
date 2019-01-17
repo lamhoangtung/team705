1dca231117ee9f23cc003e256d92c98857c14046
linus <linus@cinnamon.is>
2019-01-17 23:46:30 +0700

    All bugs fixed

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/linus/team705/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/linus/team705/devel;/opt/ros/melodic".split(';') if "/home/linus/team705/devel;/opt/ros/melodic" != "" else []
---
+++ build/team705/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/linus/team705/src/team705
+# Build directory: /home/linus/team705/build/team705
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
