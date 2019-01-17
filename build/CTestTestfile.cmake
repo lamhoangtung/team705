81c462e6a1b648dc382fc3039f458a7741bd5fc6
linus <linus@cinnamon.is>
2019-01-17 23:47:48 +0700

    Remove old files

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/linus/team705/src
-# Build directory: /home/linus/team705/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("team705")
--- build/file_server/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/linus/team705/src/file_server
-# Build directory: /home/linus/team705/build/file_server
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
